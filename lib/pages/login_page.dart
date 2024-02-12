2b3f5851e44628790029ad722c1eec472dbbc406
Gabriel Lartey <gabriellartey16@gmail.com>
2024-02-12 12:02:33 +0000

    Initial commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  Color primaryColor = const Color.fromRGBO(125, 191, 211, 1.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
