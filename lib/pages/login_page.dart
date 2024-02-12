b39ce3bd6233fddd4872269ad2b5789339ae18c2
Gabriel Lartey <gabriellartey16@gmail.com>
2024-02-12 15:59:41 +0000

    Added some color and size variables

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,9 +5,16 @@ class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
   Color primaryColor = const Color.fromRGBO(125, 191, 211, 1.0);
+  Color secondaryColor = const Color.fromRGBO(169, 224, 241, 1.0);
+
+  double deviceHeight = 0;
+  double deviceWidth = 0;
 
   @override
   Widget build(BuildContext context) {
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
+
     return const Scaffold();
   }
 }
